cb2faff5bdeef4417480b0c4008a4297e3225f26
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-12-11 10:46:24 -0300

    test: add e2e test for confirmUserSignup

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -1,3 +1,5 @@
+const AWS = require('aws-sdk')
+
 const we_invoke_confirmUserSignup = async (username, name, email) => {
     const handler = require('../../functions/confirm-user-signup').handler
 
@@ -25,6 +27,38 @@ const we_invoke_confirmUserSignup = async (username, name, email) => {
 
 }
 
+const a_user_signs_up = async (password, name, email) => {
+    const cognito = new AWS.CognitoIdentityServiceProvider()
+    const userPoolId = process.env.CognitoUserPoolId
+    const clientId = process.env.WebCognitoUserPoolClientId
+    
+    const response = await cognito.signUp({
+        ClientId: clientId,
+        Username: email,
+        Password: password,
+        UserAttributes: [
+            { Name: 'name', Value: name }
+        ]
+    }).promise()
+
+    const username = response.UserSub
+    console.log(`[${email}] - user has signed up [${username}]`)
+
+    await cognito.adminConfirmSignUp({
+        UserPoolId: userPoolId,
+        Username: username
+    }).promise()
+
+    console.log(`[${email}] - confirmed sign up`)
+
+    return {
+        username,
+        name,
+        email
+    }
+}
+
 module.exports = {
-    we_invoke_confirmUserSignup
+    we_invoke_confirmUserSignup,
+    a_user_signs_up
 }
