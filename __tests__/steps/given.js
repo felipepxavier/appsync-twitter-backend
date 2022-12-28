c37a5ea642c884a0bff5dafe6ff94dae4fd1fbe8
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2022-12-27 22:51:25 -0300

    test(e2e): getMyProfile query

--- __tests__/steps/given.js
+++ __tests__/steps/given.js
@@ -1,4 +1,5 @@
 const chance = require('chance').Chance()
+const AWS = require('aws-sdk')
 const velocityUtil = require('amplify-appsync-simulator/lib/velocity/util')
 const a_random_user = () => {
     const firstName = chance.first({ nationality: 'en' })
@@ -31,7 +32,54 @@ const an_appsync_context = (identity, args) => {
     }
 }
 
+const an_authenticated_user = async () => {
+    const { name, email, password } = a_random_user()
+    const cognito = new AWS.CognitoIdentityServiceProvider()
+    
+    const userPoolId = process.env.CognitoUserPoolId
+    const clientId = process.env.WebCognitoUserPoolClientId
+    
+    const response = await cognito.signUp({
+        ClientId: clientId,
+        Username: email,
+        Password: password,
+        UserAttributes: [
+            { Name: 'name', Value: name }
+        ]
+    }).promise()
+
+    const username = response.UserSub
+    console.log(`[${email}] - user has signed up [${username}]`)
+
+    await cognito.adminConfirmSignUp({
+        UserPoolId: userPoolId,
+        Username: username
+    }).promise()
+
+    console.log(`[${email}] - confirmed sign up`)
+
+    const auth = await cognito.initiateAuth({
+        AuthFlow: 'USER_PASSWORD_AUTH',
+        ClientId: clientId,
+        AuthParameters: {
+            USERNAME: username,
+            PASSWORD: password
+        }
+    }).promise()
+
+    console.log(`[${email}] - signed in`)
+    return {
+        username,
+        name,
+        email,
+        idToken: auth.AuthenticationResult.IdToken,
+        accessToken: auth.AuthenticationResult.AccessToken
+    }
+
+}
+
 module.exports = {
     a_random_user,
-    an_appsync_context
+    an_appsync_context,
+    an_authenticated_user
 }
